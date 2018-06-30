ee89bfcb461338d27ce852c95b263a0fd9b24dac
LilliamHaro <qualia950@gmail.com>
2018-06-30 01:17:09 -0500

    subiendo primera seccion

--- js/app.js
+++ js/app.js
@@ -6,19 +6,19 @@ wow = new WOW({
 wow.init();
 
 $(document).ready(function() {
-  var scroll = function() {
-    if ($(document).scrollTop() > 60) {
-        $('.nav-2').removeClass('mg-top-100');
-      $('.nav-2').removeClass('hideme');
-      // $('.navbar-fixed-top').removeClass('height-10');
-    } else {
-        $('.nav-2').addClass('mg-top-100');
-      $('.nav-2').addClass('hideme');
-      // $('.navbar-fixed-top').addClass('height-10');
-    }
-  };
-  scroll();
-  $(window).scroll(function() {
-    scroll();
-  });
+  // var scroll = function() {
+  //   if ($(document).scrollTop() > 60) {
+  //       $('.nav-2').removeClass('mg-top-100');
+  //     $('.nav-2').removeClass('hideme');
+  //     // $('.navbar-fixed-top').removeClass('height-10');
+  //   } else {
+  //       $('.nav-2').addClass('mg-top-100');
+  //     $('.nav-2').addClass('hideme');
+  //     // $('.navbar-fixed-top').addClass('height-10');
+  //   }
+  // };
+  // scroll();
+  // $(window).scroll(function() {
+  //   scroll();
+  // });
 });
